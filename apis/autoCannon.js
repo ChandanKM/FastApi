925ddc77edc11974eceb503215dc07d52cd4d441
ChandanKM <53951383+ChandanKM@users.noreply.github.com>
2020-01-09 19:30:05 +0530

    Last changes for master

--- apis/autoCannon.js
+++ apis/autoCannon.js
@@ -1,19 +1,30 @@
 'use strict'
 const autocannon = require('autocannon')
 autocannon({
-  url: 'http://202.66.172.230/LexygenPortal/',
+  url: 'https://jsonplaceholder.typicode.com/posts',
   connections: 50, //default
   pipelining: 1, // default
-  duration: 10 // default
+  duration: 10, // default
+  method:'POST',
+  title:'Demo Title',
+  body:"{'userId': 1,'id': 1,'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit','body': 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'}"
 }, console.log)
  
 // async/await
 async function foo () {
-  const result = await autocannon({
-    url: 'http://202.66.172.230/LexygenPortal/',
-    connections: 50, //default
-    pipelining: 1, // default
-    duration: 10 // default
-  })
-  console.log(result)
+  const autocannon = require('autocannon')
+autocannon({
+  url: 'https://jsonplaceholder.typicode.com/posts',
+  connections: 50, //default
+  pipelining: 1, // default
+  duration: 10, // default
+  method:'POST',
+  title:'Demo Title',
+  requests:{
+    userId:1,
+    id:1,
+    title:'Title',
+    body:'body'
+  }
+}, console.log)
 }
